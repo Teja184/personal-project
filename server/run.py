f04a929f99855d9445a4719d3e578b0d3c41c014
Teja184 <gtejavardhanreddy974@gmail.com>
2025-03-06 21:33:26 +0530

    creating deployment server

--- server/run.py
+++ server/run.py
@@ -4,5 +4,7 @@ if __name__ == "__main__":
     server = create_app()
     server.run(debug=True)
 
-
+else:
+    server = create_app()
+    
 # https://chatgpt.com/share/678f3871-0d24-8013-88d6-92495c4ae41c
